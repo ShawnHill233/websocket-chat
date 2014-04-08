7637b56db20d74f6c2126ce2c635834ec92ebbd6
Jorge Calás Lozano <calas@qvitta.net>
2014-04-08 20:37:48 +0200

    Update deploy recipes

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,28 +22,24 @@ after "deploy:migrations", "deploy:cleanup"
 after "deploy:update_code", "deploy:symlink_configs"
 
 namespace :deploy do
+  desc "Tasks to execute after code update"
+  task :symlink_configs, :roles => [:app] do
+    # run "ln -nfs #{deploy_to}/#{shared_dir}/config/database.yml #{release_path}/config/database.yml"
+    run "if [ -d #{release_path}/tmp ]; then rm -rf #{release_path}/tmp; fi; ln -nfs #{deploy_to}/#{shared_dir}/tmp #{release_path}/tmp"
+  end
+
   desc "Start the Thin processes"
   task :start do
-    run  <<-CMD
-      cd #{current_path}; bundle exec thin start -C config/thin.yml
-    CMD
+    run  "thin start -C /etc/thin/#{application}.yml"
   end
 
   desc "Stop the Thin processes"
   task :stop do
-    run <<-CMD
-      cd #{current_path}; bundle exec thin stop -C config/thin.yml
-    CMD
+    run "thin stop -C /etc/thin/#{application}.yml"
   end
 
   desc "Restart the Thin processes"
   task :restart do
     run "thin restart -C /etc/thin/#{application}.yml"
-  end
-
-  desc "Tasks to execute after code update"
-  task :symlink_configs, :roles => [:app] do
-    run "ln -nfs #{deploy_to}/#{shared_dir}/config/database.yml #{release_path}/config/database.yml"
-    run "if [ -d #{release_path}/tmp ]; then rm -rf #{release_path}/tmp; fi; ln -nfs #{deploy_to}/#{shared_dir}/tmp #{release_path}/tmp"
   end
 end
